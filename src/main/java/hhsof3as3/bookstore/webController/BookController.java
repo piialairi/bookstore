71d4f29b9ee9de10ead1066fec7173cbbdbe0ab3
piialairi <piia.lairi@myy.haaga-helia.fi>
2023-02-08 11:03:02 +0200

    Adding add and delete functions

--- src/main/java/hhsof3as3/bookstore/webController/BookController.java
+++ src/main/java/hhsof3as3/bookstore/webController/BookController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -29,4 +30,27 @@ public class BookController {
 		//model.addAttribute("books", bookRepository.findAll());
 		return "booklist"; // .html
 	}
+	
+	// lisäys
+	@RequestMapping(value = "/addbook")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook"; // html
+	}
+	
+	// tallenna
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveBook(Book book) {
+		bookRepository.save(book);
+		return "redirect:booklist";
+	}
+	
+	
+	// kirjan poisto
+	@RequestMapping(value = "delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		bookRepository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+		
 }
